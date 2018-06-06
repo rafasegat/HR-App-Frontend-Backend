8e7eeabf9eb987289761526d9fe21231939aad4c
Rafael Souza <rafasegat@gmail.com>
2018-06-06 17:48:41 +1000

    Some fields

--- client/app/components/Form/RenderField.js
+++ client/app/components/Form/RenderField.js
@@ -4,12 +4,14 @@ const RenderField = ({
     input,
     label,
     type,
+    description,
     meta: { touched, error, warning }
   }) => (
     <div className="input-row">
       <label>{label}</label>
       <div className="input-field">
         <input  {...input} type={type} />
+        {description && <span className="description">{description}</span>}
         {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
       </div>
     </div>
